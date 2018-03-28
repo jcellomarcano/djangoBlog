62b38ad3307b154c55acd7488f5efc55f62d34fe
jcellomarcano <12-10359@usb.ve>
2018-03-28 12:56:58 -0400

    hey

--- jcelloProject/danisite/daniblog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DaniblogConfig(AppConfig):
-    name = 'daniblog'
--- jcelloProject/jcello/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jcellomarcano/jcelloProject/jcello/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
